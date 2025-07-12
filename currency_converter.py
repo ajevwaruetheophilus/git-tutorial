9d877c19829c98cb68b4237d11c3982446668f6d
Theophilus Ajevwarue <ajevwaruetheophilus@gmail.com>
2025-07-12 20:11:30 +0100

    currency converter formatted

--- currency_converter.py
+++ currency_converter.py
@@ -1,5 +1,5 @@
 # Program converts dollars to naira
-dollar_amount = float(input("Enter amount in dollar "))
+dollar_amount = float(input("Enter amount in dollar: "))
 conversion_unit = 1600
 naira_amount = dollar_amount * conversion_unit
-print(f"{dollar_amount} dollars is equivalent to {naira_amount:.2f} naira") 
+print(f"${dollar_amount:.2f} dollars is equivalent to #{naira_amount:.2f}") 
